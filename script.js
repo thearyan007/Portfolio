2d0cf0cb0fa0cbb987485aeb7c69548a5f67e660
thearyan007 <thearyangupta007@gmail.com>
2024-12-15 01:06:08 +0530

    added newer changes

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ const toggleMenu = () => {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 };
+function toggleDetails(button) {
+  // Find the corresponding project details by traversing the DOM from the button
+  var details = button
+    .closest(".details-container")
+    .querySelector(".project-details");
+
+  // Toggle the visibility of the project details
+  if (details.style.display === "none") {
+    details.style.display = "block";
+  } else {
+    details.style.display = "none";
+  }
+}
